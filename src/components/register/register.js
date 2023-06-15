dd50f1671d41fef0703d73f0d899c03eb8d971c2
AKMoralesRamos <dental.morak@gmail.com>
2023-06-15 13:41:32 -0600

    cambios-register-controller

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -1,10 +1,15 @@
+import { crearUsuario } from './register.controller';
+
 const register = {
   loadHTML: async () => {
     const templateHTML = await fetch('components/register/register.html');
     return templateHTML.text();
   },
   loadEvents: async () => {
-
+    document.getElementById('Crearcuenta').addEventListener('click', async () => {
+      document.getElementById('Crearcuenta').disabled = true;
+      await crearUsuario({ nombre: 'Karen', email: 'love@gmail.com', edad: '18' });
+    });
   },
 };
 
