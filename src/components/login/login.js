61e1e30748a3d115dbe16ef0491a623f3eab73f9
Luz Vazquez <vgluzmaria96@gmail.com>
2023-06-14 20:21:47 -0600

    modificaciones de ruteo

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -4,7 +4,14 @@ const login = {
     return templateHTML.text();
   },
   loadEvents: async () => {
+    /* -------------------------- Navegacion a register ------------------------- */
+    document.getElementById('spnNuevaCuenta').addEventListener('click', () => {
+      window.history.pushState({}, '', `${window.location.origin}/register`);
+      /* ----- Dispara manualmente el evento popstate para actualizar la ruta ----- */
+      window.dispatchEvent(new PopStateEvent('popstate'));
+    });
 
+    /* ----------------------------- Inniciar sesion ---------------------------- */
   },
 };
 
