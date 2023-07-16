fb9cb35131ac92411807e4e8e7d845f18ab1642c
AKMoralesRamos <dental.morak@gmail.com>
2023-07-16 17:48:56 -0600

    Inicio test

--- test/index.spec.js
+++
@@ -1,9 +0,0 @@
-// importamos la funcion que vamos a testear
-/* import { feedMock } from '../src/components/feed/feed.mock'; */
-import { updatePostLike } from '../src/components/feed/feed.controller';
-
-describe('updatePostLike', () => {
-  it('debería ser una función', () => {
-    expect(typeof updatePostLike).toBe('function');
-  });
-});
